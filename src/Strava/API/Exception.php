c1dd3b73fb056c14f9d5a955d997b292cd4bd3a0
Trickytree1984 <65119891+Trickytree1984@users.noreply.github.com>
2020-05-10 15:34:50 +0100

    Add files via upload

---
+++ src/Strava/API/Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace Strava\API;
+
+/**
+ * Catch me please!
+ *
+ * @author Bas van Dorst
+ * @package StravaPHP
+ */
+class Exception extends \Exception
+{
+}
---
+++ src/Strava/API/Service/Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace Strava\API\Service;
+
+/**
+ * Catch me please!
+ *
+ * @author Bas van Dorst
+ * @package StravaPHP
+ */
+class Exception extends \Exception
+{
+}
